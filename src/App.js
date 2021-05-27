80cc57193f9ab709e37a6ad10f79e76d6cab18df
ErickCruz3 <30erickC@gmail.com>
2021-05-27 11:56:07 -0500

    cuarto commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './componentes/Expenses'
+import React from 'react'
 
 function App() {
   const expenses = [
@@ -26,12 +27,19 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ]
-  return (
+
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, "Let's get started!"),
+    React.createElement(Expenses, { items: expenses})
+  )
+  /*return (
     <div>
       <h2>Let's get started!</h2>
       <Expenses items={expenses}></Expenses>
     </div>
-  );
+  );*/
 }
 
 export default App;
