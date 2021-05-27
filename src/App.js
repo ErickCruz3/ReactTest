0846fefb210dd2ee2e7890609c3392c2a771b586
ErickCruz3 <30erickC@gmail.com>
2021-05-27 08:54:53 -0500

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import ExpenseItem from './componentes/ExpenseItem'
+import Expenses from './componentes/Expenses'
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Papel Toalla',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', 
+    title: 'Nueva TV', 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12),
+    },
+    {
+      id: 'e3',
+      title: 'Seguro de Auto',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'Escritorio Nuevo(Madera)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
